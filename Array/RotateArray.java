aa004f719f1a48e9674bdf14fbd242e8650f344f
dhirajsahh <dhirajmahato2019@gmail.com>
2024-05-30 20:30:43 +0545

    array problem added

--- Array/RotateArray.java
+++ Array/RotateArray.java
@@ -1,14 +1,29 @@
-
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
 public class RotateArray {
     static void rotateArray(int a[],int target){
-
+           int n=a.length;
+           int k=target%n;
+           reverseArray(a,0,n-k-1);
+           reverseArray(a, n-k, n-1);
+           reverseArray(a, 0, n-1);
+    }
+    static void reverseArray(int []a,int start,int end){
+      while(start<end){
+        int temp=a[end];
+        a[end]=a[start];
+        a[start]=temp;
+        start++;
+        end--;
+      } 
     }
     public static void main(String[] args) {
-        int []a={10,3,2,52,3};
-        int target=4;
-        int n=a.length;
-        for(int i=0;i<n;i++){
-            System.out.print(a[i]+" ");
+        int []a={1,2,3,4,5,6,7};
+        int target=3;
+        rotateArray(a, target);
+        for(int e:a){
+            System.out.print(e+" ");
         }
+        
     } 
 }
--- Recursion/basic.java
+++ Recursion/basic.java
@@ -16,11 +16,24 @@ public class basic {
         int ans=pow(a,b-1)*a;
         return ans;
     }
-   
+    //Displaying array using recursion
+    static void displayarray(int arr[],int n){
+       
+        
+        if(n==0){
+            return ;
+        }
+        System.out.println(arr[n-1]);
+        displayarray(arr,n-1);
+
+    }
 
     public static void main(String[] args) {
       // printNaturalNumber(5);
        //System.out.println(sumofnaturalNumber(15));
-       System.out.println(pow(4,3));
+       //System.out.println(pow(4,3));
+       int arr[]={1,2,4,5,6};
+       int n=5;
+       displayarray(arr,n);
     }
 }
