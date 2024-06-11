f06eb5ac36472e591517e59c1a9a4b1882002a9a
dhirajsahh <dhirajmahato2019@gmail.com>
2024-06-11 23:50:09 +0545

    valid parenthese and Duplicate parenthese

---
+++ Stack/level1.java
@@ -0,0 +1,59 @@
+
+import java.util.Stack;
+
+public class level1 {
+     
+    public static boolean DuplicateBrackets(String str){
+
+        Stack<Character>s=new Stack<>();
+        int i=-1;
+        while(i<str.length()-1){
+            i++;
+            char ch=str.charAt(i);
+            if(ch!=')'){
+                s.push(ch);
+            } 
+            else {
+                if(s.peek()=='(') return false;
+                else {
+                    while(s.peek()!='('){
+                        s.pop();
+                    }
+                }
+                s.pop();
+            }
+        }
+        return true;
+    }
+    public static boolean isValid(String s){
+
+        Stack<Character>stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                stack.push(ch);
+            }
+            else {
+                if(stack.isEmpty()) return false;
+                else {
+                    if((stack.peek()=='('&&ch==')')||(stack.peek()=='{'&&ch=='}')||(stack.peek()=='['&&ch==']')){
+                        stack.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+        if(stack.isEmpty()) return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s="((a+b)+(c+d))";
+        String st="(a+b)+((c+d))";
+        boolean result=DuplicateBrackets(st);
+        System.out.println(result);
+        System.out.println(isValid("()[]{}"));
+    }
+}
